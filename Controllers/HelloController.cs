1b9ed134d98fa8612169333d99f7600ea6092000
MatthiasDesmoulins <Matthias-Externe.Desmoulins@asmeg.org>
2024-05-19 06:12:30 +0200

    Ajoutez des fichiers projet.

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class HelloController
+    {
+        [HttpGet]
+        [Route("/Say")] // The endpoint here is reached by calling `/Say` route
+        public String SayHello()
+        {
+            return "Hello !";
+        }
+        [HttpGet]
+        [Route("/Yell")] // The endpoint here is reached by calling `/Yell` route
+        public String YellHello()
+        {
+            return "HELLO !";
+        }
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+
+namespace WebApplication3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+            app.UseRouting();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Hello}/{action=Say}/{id?}");
+            
+
+        app.Run();
+        }
+    }
+}
